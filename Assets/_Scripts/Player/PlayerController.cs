22240b56699c1d91fd8de147568c55f6f85973c9
HéctorTL <78750635+HTLGames@users.noreply.github.com>
2025-04-08 18:51:36 +0200

    Movement basics

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
 
-public class PlayerController : Singleton<PlayerController>
+public class PlayerController : MonoBehaviour// Singleton<PlayerController>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    // Dependencies
+    private PlayerMovement _playerMovement;
+
+    // Components
+    private Animator _anim;
+
+    void OnEnable()
     {
         
     }
--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    // Parameters
+    [SerializeField] private float _playerSpeed = 1f;
+    [SerializeField] private float _movementSmooth = 2f;
+
+    // Input
+    private PlayerInput _input;
+    private InputAction _moveAction;
+    private Vector2 _movementVector;
+    
+    // Components
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    // Properties
+    /// <summary>
+    /// Gets the magnitude of the player's velocity in a range of [0,1]
+    /// </summary>
+    public float movementAmount {
+        get => _movementVector.magnitude / _playerSpeed;
+    }
+    public bool isMoving {
+        get => _movementVector != Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        // Setup components
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        // Setup input actions
+        _input = GetComponent<PlayerInput>();
+        _moveAction = _input.actions["Move"];
+    }
+
+    void FixedUpdate()
+    {
+        // Smooth out movement
+        _movementVector = Vector2.Lerp(_movementVector, _moveAction.ReadValue<Vector2>() * _playerSpeed, 
+            Time.fixedDeltaTime * _movementSmooth);
+        rb.linearVelocity = _movementVector;
     }
 }
